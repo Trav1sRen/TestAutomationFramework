e7679cfa00bd6321206059f7cae71b763669aeee
zhuang.ren <Zhuang.ren@openjawtech.com>
2020-06-10 17:51:29 +0800

    Add new methods.

--- src/objects/api_base_obj.py
+++ src/objects/api_base_obj.py
@@ -4,22 +4,74 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(level
 logger = logging.getLogger(__name__)
 
 import re
-
-from abc import abstractmethod
-
 import lxml.etree as et
+import xmltodict
+from abc import ABCMeta, abstractmethod
 
 from ..utils.common_utils import proj_root, encoding, typeassert
 from .base_obj import BaseObject
 
 
+class CustomDict(dict):
+    def __getitem__(self, key):
+        """
+        Overwrite __getitem__ to return None instead of 'KeyError' when key has no mapping
+        :param key: key used for mapping
+        :return: mapping of the key (None if the key has no mapping)
+        """
+
+        try:
+            val = super().__getitem__(key)
+            if isinstance(val, dict):
+                return CustomDict(val)
+            elif isinstance(val, list):
+                return list(map(lambda e: CustomDict(e) if isinstance(e, dict) else e, val))
+            else:
+                return val
+        except KeyError:
+            return None
+
+
 def set_attribute_for_node(ele, attr_dict):
+    """
+    Set attributes for specific node
+    :param ele: xml node to set the attributes
+    :param attr_dict: attribute dict to append onto the node
+    :return: None
+    """
+
     if attr_dict:
         for key, val in attr_dict.items():
             ele.set(key, val)
 
 
-@typeassert(str or bytes)
+def strip_ns_prefix(bytes_str):
+    """
+    Remove the namespace of xml string
+    :param bytes_str: xml string in bytes about to be processed
+    :return: xml string without namespaces
+    """
+
+    root = et.fromstring(bytes_str)
+    for ele in root.xpath('descendant-or-self::*'):
+        if ele.prefix:
+            ele.tag = et.QName(ele).localname
+    return et.tostring(root, encoding=encoding)
+
+
+@typeassert(bytes)
+def convert_xml_to_dict(bytes_str):
+    """
+    Convert xml string to dict
+    :param bytes_str: xml string in bytes about to be processed
+    :return: instance of CustomDict
+    """
+
+    new_str = strip_ns_prefix(bytes_str)
+    d = xmltodict.parse(new_str)
+    return CustomDict(d)
+
+
 def validate_schema(rs_body, schema_name, path='/schema/'):
     # open and read schema file
     with open(proj_root + path + schema_name + '.xsd', encoding=encoding) as schema_file:
@@ -51,21 +103,16 @@ def validate_schema(rs_body, schema_name, path='/schema/'):
                 error_log_file.write(str(xmlschema.error_log))
 
 
-class APIBaseObject(BaseObject):
-    rq_body = None
-    rs_body = None
+class APIBaseObject(BaseObject, metaclass=ABCMeta):
+    rq_body, headers = None, None  # base attributes for a request
 
     soap_skin = None  # not overwriting if pure xml other than SOAP request
 
-    @abstractmethod
-    def set_headers(self):
-        pass
-
-    @typeassert(d=dict)
-    def assemble_request_xml(self, rq_name, d, **root_attrs):
+    @typeassert(rq_dict=dict)
+    def assemble_request_xml(self, rq_name, rq_dict, **root_attrs):
         root = et.Element(rq_name, **root_attrs)
 
-        for key, value in d.items():
+        for key, value in rq_dict.items():
             cur_ele = root
             for node in key.split('.'):
                 attr_dict = {}
@@ -100,3 +147,8 @@ class APIBaseObject(BaseObject):
             cur_ele.text = value
 
         self.rq_body = self.soap_skin % (et.tostring(root, encoding='unicode'))
+
+    @abstractmethod
+    @typeassert(rs_dict=dict)
+    def save_response(self, rs_dict):
+        pass
