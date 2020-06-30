89a5f03b1c101519125d1d48f4186cfd6088e24e
Travis <1760003658@qq.com>
2020-06-30 22:57:04 +0800

    Automate the class of AndroidDevices.

--- taf/utils/__init__.py
+++ taf/utils/__init__.py
@@ -1,4 +1,4 @@
-from .common_utils import CustomDict
-from .common_utils import encoding, var_dict, proj_root
-from .common_utils import typeassert, xml2dict, validate_schema
+from .api_utils import CustomDict
+from .api_utils import encoding, var_dict, proj_root
+from .api_utils import typeassert, xml2dict, validate_schema
 from .web_utils import fluent_wait, check_os
---
+++ taf/utils/app_utils/android.py
@@ -0,0 +1,24 @@
+import os
+import re
+
+
+class AndroidDevices:
+    def __init__(self):
+        self.udids = []
+        for line in os.popen('adb devices').readlines()[1:-1]:
+            udid = re.findall(r'^\w*\b', line)[0]
+            self.udids.append(udid)
+
+    @property
+    def versions(self):
+        """ Get the os version of connected devices """
+
+        return list(map(lambda udid: os.popen(
+            'adb -s %s shell getprop ro.build.version.release' % udid).readlines()[0].strip('\n'), self.udids))
+
+    @property
+    def device_names(self):
+        """ Get the product name of connected devices """
+
+        return list(map(lambda udid: os.popen(
+            'adb -s %s shell getprop ro.product.model' % udid).readlines()[0].strip('\n'), self.udids))
