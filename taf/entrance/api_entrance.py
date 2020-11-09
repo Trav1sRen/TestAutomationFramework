79d5ff096360d64e76907f3627fa87d071aaeef4
zhuang.a.ren <zhuang.a.ren@rakuten.com>
2020-11-09 18:56:53 +0800

    Update the function to check schema

--- taf/entrance/api_entrance.py
+++ taf/entrance/api_entrance.py
@@ -1,5 +1,5 @@
 from ..clients.api import SoapBaseClient
-from ..utils import typeassert
+from ..utils import typeassert, validate_schema
 
 
 class ApiEntrance:
@@ -7,8 +7,9 @@ class ApiEntrance:
         module = __import__('src.objects.api.' + module_name, fromlist=[''])  # 'fromlist' arg is not an empty list
         self.api_obj = getattr(module, cls_name)(*args, **kwargs)
 
-    @typeassert(extra_headers=dict)
-    def dispatch_soap_request(self, verify_ssl=False, extra_headers=None, **xml_args):
+    @typeassert(schema_name=str, extra_headers=dict)
+    def dispatch_soap_request(self, verify_ssl=False, check_schema=False, schema_name=None, extra_headers=None,
+                              **xml_args):
         self.api_obj.construct_xml(soap=True, **xml_args)
 
         extra_headers = extra_headers or {}
@@ -17,6 +18,12 @@ class ApiEntrance:
 
         client = SoapBaseClient(verify_ssl=verify_ssl)
         client.send_req(self.api_obj.url, self.api_obj.default_headers, self.api_obj.rq_body)
+
+        if check_schema:
+            schema_name = schema_name or ''
+            if schema_name == '':
+                raise ValueError('schema name cannot be an empty string')
+            validate_schema(client.rs_body, schema_name)
 
         self.api_obj.load_client_response(client.rs_body)
         self.api_obj.process_response()
