bb4a104bc82aa7deb8de26b8fdb334a2810bafbc
Travis <1760003658@qq.com>
2020-11-10 22:32:22 +0800

    Small improvements.

--- taf/clients/api/rest_base_client.py
+++ taf/clients/api/rest_base_client.py
@@ -15,9 +15,11 @@ logger = logging.getLogger(__name__)
 class RestBaseClient(APIBaseClient):
     @typeassert(rq_body=str, params=dict)
     def send_req(self, method, url, headers, rq_body=None, params=None):
+        rq_body = rq_body or ''
+        params = params or {}
+
         logger.info('*********************** REQUEST START ***********************')
 
-        params = params or {}
         url = url + '?' + '&'.join(
             '%s=%s' % (k, v) for k, v in params.items()) if params else url
         logger.info('%s to <%s>' % (method, url))
@@ -31,11 +33,13 @@ class RestBaseClient(APIBaseClient):
             # request str is in json format
             logger.info('Request Body: \n' + rq_body)
 
-        if params:
-            response = requests.request(method, url, headers=headers, verify=self.verify_ssl)
-        else:
+        response = None
+        if rq_body:
             response = requests.request(method, url, headers=headers, data=rq_body,
                                         verify=self.verify_ssl)
+        if params:
+            response = requests.request(method, url, headers=headers, verify=self.verify_ssl)
+
         logger.info('***********************  REQUEST END  ***********************')
 
         self.output_response(response)
